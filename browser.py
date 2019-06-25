8e1ae32b4320b8546649adaab686cf84871ea6e4
Arthur Milchior <Arthur@Milchior.fr>
2019-06-25 23:41:15 +0200

    Correct #3

--- browser.py
+++ browser.py
@@ -2,11 +2,12 @@ from PyQt5.QtWidgets import QAction
 from anki.hooks import addHook
 from aqt.exporting import ExportDialog
 from aqt.qt import QKeySequence
+from aqt import mw
 
 def setupMenu(browser):
     a = QAction("Export selection", browser)
     a.setShortcut(QKeySequence("Ctrl+Shift+E"))
-    a.triggered.connect(lambda : ExportDialog(mw,cids=browser.selectedCards()))
+    a.triggered.connect(lambda : ExportDialog(mw, cids=browser.selectedCards()))
     browser.form.menuEdit.addAction(a)
 
 addHook("browser.setupMenus", setupMenu)
