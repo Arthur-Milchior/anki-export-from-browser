4bc022c515ee91e006d8460b15c87cb290842e11
Arthur Milchior <Arthur@Milchior.fr>
2020-01-24 17:50:42 -0800

    put export in the "card" menu

--- browser.py
+++ browser.py
@@ -5,11 +5,13 @@ from aqt import mw
 from aqt.exporting import ExportDialog
 from aqt.qt import QKeySequence
 
+# Adding action to menu
+
 
 def setupMenu(browser):
     a = QAction("Export selection", browser)
     a.setShortcut(QKeySequence("Ctrl+Shift+E"))
     a.triggered.connect(lambda : ExportDialog(mw, cids=browser.selectedCards()))
-    browser.form.menuEdit.addAction(a)
+    browser.form.menu_Cards.addAction(a)
 
 addHook("browser.setupMenus", setupMenu)
